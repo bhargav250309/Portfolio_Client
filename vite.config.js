8e5ba65f82a984804accd1a17e3edbf6a6f3eca2
bhargav250309 <bhargavjotangiya12@gmail.com>
2025-04-02 15:06:29 +0530

    Fix TinyMCE domain issue

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: '/', // Adjust if deployed in a subdirectory
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Adjust if deployed in a subdirectory
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+  server: {
+    proxy: {
+      '/tinymce': 'https://cdn.tiny.cloud',
     },
+  },
+  build: {
+    target: 'esnext',
   },
 });

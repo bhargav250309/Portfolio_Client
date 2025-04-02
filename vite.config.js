4a1241819eea116d5892645ac005f4e07c3a4b92
bhargav250309 <bhargavjotangiya12@gmail.com>
2025-04-02 16:00:36 +0530

    finel commit

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/app/',
   server: {
     proxy: {
       '/tinymce': 'https://cdn.tiny.cloud',
